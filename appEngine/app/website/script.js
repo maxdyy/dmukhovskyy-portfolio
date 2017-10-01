304ea4f29be8e5f66eac0be785c422f4f5a34670
Maksym Dmukhovskyy <maksymdmukhovskyy@MacBook-Air-di-Maksym.local>
2017-10-01 22:57:32 +0100

    Avatar scroll animation

--- appEngine/app/website/script.js
+++ appEngine/app/website/script.js
@@ -1,3 +1,17 @@
 // particlesJS.load('particles-js', 'assets/particles/particles.json', function() {
 //   console.log('callback - particles.js config loaded');
 // });
+
+var $window = $(window);
+var $intro = $('#intro');
+var $menu = $('#menu');
+
+$window.scroll(function() {
+  if ($window[0].scrollY >= $intro[0].clientHeight) {
+    $menu.addClass('mini');
+    $intro.addClass('mini');
+  } else {
+    $menu.removeClass('mini');
+    $intro.removeClass('mini');
+  }
+});
