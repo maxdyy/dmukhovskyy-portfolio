2a7e3b51a203ef401d2ae72a4b51d0f94b9b199d
Maksym <maxlearncode@outlook.com>
2018-01-25 23:07:51 +0100

    SMART approach desctop section with styled cards.

--- appEngine_v2.0/app/website/components/init/script.js
+++ appEngine_v2.0/app/website/components/init/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
   $(".button-collapse").sideNav();
   $('.modal').modal();
+  $('.scrollspy').scrollSpy();
 })
+
+particlesJS.load('particles-js', 'assets/particles/particles.json', function() {});
