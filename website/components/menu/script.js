b9959cbac28617e0a6e0400533837322912a6c16
Maksym Dmukhovskyy <dm.maksym@gmail.com>
2018-05-19 20:42:56 +0200

    Removed menu localization since content too specific to be translated (like smart section)

--- website/components/menu/script.js
+++ website/components/menu/script.js
@@ -2,33 +2,3 @@
 $('.close-click').on('click', function() {
   $('side-nav').sideNav('hide');
 })
-
-// dropdown localization logic
-var locBtn = $('.button-localization');
-var locBtnMob = $('.button-localization-mobile');
-var locOption = $('.localization-option');
-var locOptionMob = $('.localization-option-mobile');
-
-locOption.on('click', function() {
-  var locOptionTxt = $(this).text();
-  var locBtnTxt = locBtn.text();
-
-  $(this).text(locBtnTxt);
-  $('.localization-option-mobile:contains(' + locOptionTxt +')').text(locBtnTxt);
-  locBtn.text(locOptionTxt);
-  locBtnMob.text(locOptionTxt);
-
-  $('body').removeClass(locBtnTxt).addClass(locOptionTxt);
-})
-
-locOptionMob.on('click', function() {
-  var locOptionMobTxt = $(this).text();
-  var locBtnMobTxt = locBtnMob.text();
-
-  $(this).text(locBtnMobTxt);
-  $('.localization-option:contains(' + locOptionMobTxt +')').text(locBtnMobTxt);
-  locBtn.text(locOptionMobTxt);
-  locBtnMob.text(locOptionMobTxt);
-
-  $('body').removeClass(locBtnMobTxt).addClass(locOptionMobTxt);
-})
