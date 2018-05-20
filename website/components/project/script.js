c1d2a27d647bbe9b27f8801ef7591331a9fbffbb
Maksym Dmukhovskyy <dm.maksym@gmail.com>
2018-05-20 18:51:20 +0200

    Graphcms. GraphQL setup for projects page

--- website/components/project/script.js
+++ website/components/project/script.js
@@ -2,8 +2,79 @@ $(document).ready(function() {
   $(".button-collapse").sideNav();
   $('.scrollspy').scrollSpy();
   $('.collapsible').collapsible();
+  getData(Graph_API, query);
 })
 
-var API_END_POINT = 'projects';
+var Graph_API = 'https://api.graphcms.com/simple/v1/dmukhovskyy';
+var query = "{\
+               allProjects \
+                { isPublished \
+                  id \
+                  projectTitle \
+                  headerBackground\
+                  projectLogo \
+                  { id \
+                    url \
+                  } \
+                  projectURL \
+                  projectFrontEnd \
+                  projectBackEnd \
+                  projectDescription \
+                } \
+              }";
 
-var projects = getData(API, API_END_POINT);
+var projects = [];
+
+function getData(API, query) {
+  if($('body').hasClass('projects')) {
+    $.post({
+      url: API,
+      data: JSON.stringify({ "query": query }),
+      contentType: 'application/json'
+    }).done(function(response) {
+      projects = response.data.allProjects;
+      createProjects(projects);
+    });
+  };
+}
+
+var projectsContainer = $('.projects-container');
+
+function createProjects(projects) {
+  $.each(projects, function(index, project) {
+    if(project.isPublished) {
+      projectsContainer.append('\
+      <li class="project-item">\
+        <div class="collapsible-header header-'+ project.id +'">\
+          <div class="project-icon icon-'+ project.id +'"></div>\
+          <span>'+ project.projectTitle +'</span>\
+        </div>\
+        <div class="collapsible-body">\
+          <div class="project-stack">\
+            <div class="front">\
+              <span class="desc">Front-end:</span>\
+              <span>'+ project.projectFrontEnd +'</span>\
+          </div>\
+            <div class="back">\
+              <span class="desc">Back-end:</span>\
+              <span>'+ project.projectBackEnd +'</span>\
+            </div>\
+          </div>\
+          <hr class="hr-'+ project.id +'">\
+          <span class="project-desc">'+ project.projectDescription +'</span>\
+          <a class="waves-effect waves-light btn project-btn" href="' + project.projectURL + '">Visit</a>\
+        </div>\
+      </li>\
+      ');
+
+      var projectIcon = $('.icon-'+ project.id +'');
+      var projectHeader = $('.header-'+ project.id +'');
+      var projectHR = $('.hr-'+ project.id +'');
+
+      projectIcon.css({'background-image': 'url(' + project.projectLogo.url + ')'})
+      projectHeader.css({'background-image': ''+ project.headerBackground + ''})
+      projectHR.css({'background-image': ''+ project.headerBackground + ''})
+
+    }
+  })
+}
