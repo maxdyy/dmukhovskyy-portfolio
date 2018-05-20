a87cb8b9a743de617e95e628eee4be18dceb9e35
Maksym Dmukhovskyy <dm.maksym@gmail.com>
2018-05-20 12:24:24 +0200

    Projects page

--- website/components/API/script.js
+++ website/components/API/script.js
@@ -1,2 +1,10 @@
 var API = 'http://35.231.214.175/wp-json/wp/v2/';
 
+function getData(API, END_POINT) {
+  $.getJSON(API + END_POINT, function (data) {
+      console.log(data);
+    })
+    .fail(function (error) {
+      console.log(error);
+    })
+}
