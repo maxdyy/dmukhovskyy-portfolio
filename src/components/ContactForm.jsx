d3a6cdb41cf2d1c3c6ab742dc0c73a3fb71681f2
Maksym Dmukhovskyy <dm.maksym@gmail.com>
2018-08-13 22:56:13 +0200

    changed form handling to react component with fetch

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,8 +1,48 @@
 import React, { Component } from "react";
+import { Redirect } from "react-router";
 import Button from "./Button";
 
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+};
+
 class ContactForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      email: "",
+      message: "",
+      redirect: false
+    };
+  }
+
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => {
+        this.setState({ redirect: true });
+      })
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
   render() {
+    const { name, email, message, redirect } = this.state;
+
+    if (redirect) {
+      return <Redirect to="/success" />;
+    }
+
     return (
       <section className="contact-form">
         <div className="form-container">
@@ -11,7 +51,7 @@ class ContactForm extends Component {
             method="POST"
             data-netlify="true"
             netlify="true"
-            action="/success/"
+            onSubmit={this.handleSubmit}
           >
             <input type="hidden" name="form-name" value="contact" />
             <div className="input-field form-name">
@@ -20,6 +60,8 @@ class ContactForm extends Component {
                 id="form_name"
                 type="text"
                 className="validate"
+                value={name}
+                onChange={this.handleChange}
               />
               <label htmlFor="form_name">Name</label>
             </div>
@@ -29,6 +71,8 @@ class ContactForm extends Component {
                 id="form_email"
                 type="email"
                 className="validate"
+                value={email}
+                onChange={this.handleChange}
               />
               <label htmlFor="form_email">Email</label>
             </div>
@@ -37,11 +81,13 @@ class ContactForm extends Component {
                 name="message"
                 id="form_message"
                 className="materialize-textarea validate"
+                value={message}
+                onChange={this.handleChange}
               />
               <label htmlFor="form_message">Message</label>
             </div>
             <div className="row">
-              <div className="">
+              <div>
                 <Button text="submit" type="submit" />
               </div>
             </div>
